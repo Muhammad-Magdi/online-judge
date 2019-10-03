1426b9b622238aaad83ff3e56550212d4ee03b81
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2019-10-03 03:17:40 +0200

    user routers

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const problemRouter = require('./components/user/router');
 const app = express();
 
 app.use(express.json());
-app.use('./api/users', userRouter);
-app.use('./api/problems', problemRouter);
+app.use('/api/users', userRouter);
+app.use('/api/problems', problemRouter);
 
 module.exports = app;
