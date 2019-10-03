0f36fd1a0a6bfa266159168f1847f6d88077e268
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2019-10-03 16:55:22 +0200

    add the 'update' and 'delete' functions

--- components/user/controller.js
+++ components/user/controller.js
@@ -1,6 +1,7 @@
 const User = require('./model');
 const status = require('http-status');
 const validator = require('validator');
+
 module.exports = {
   create: async function(req, res) {
     const {error} = User.validate(req.body);
@@ -28,7 +29,8 @@ module.exports = {
       } else {
         user = await User.findByHandle(credential, password);
       }
-      return res.status(status.OK).json(user);
+      const token = user.generateAuthToken();
+      return res.status(status.OK).header('x-token', token).json(user);
     } catch (e) {
       return res.status(status.BAD_REQUEST).json(e);
     }
@@ -49,6 +51,37 @@ module.exports = {
         return res.status(status.NOT_FOUND).send('User Not Found');
       }
       return res.status(status.OK).json(user.getPublicData());
+    } catch (e) {
+      return res.status(status.INTERNAL_SERVER_ERROR).json(e);
+    }
+  },
+  update: async function(req, res) {
+    const nonUpdatable = ['handle', '_id'];
+    const updates = Object.keys(req.body);
+    const user = req.user;
+    const errors = [];
+    updates.forEach((update) => {
+      if (nonUpdatable.includes(update)) {
+        errors.push(update);
+      } else {
+        user[update] = req.body[update];
+      }
+    });
+    if (errors.length > 0) {
+      return res.status(status.FORBIDDEN).send(`Can't update [${errors}]`);
+    }
+    try {
+      await user.save();
+      return res.status(status.OK).json(user);
+    } catch (e) {
+      return res.status(status.INTERNAL_SERVER_ERROR).json(e);
+    }
+  },
+  delete: async function(req, res) {
+    const user = req.user;
+    try {
+      await user.remove();
+      return res.status(status.NO_CONTENT).send();
     } catch (e) {
       return res.status(status.INTERNAL_SERVER_ERROR).json(e);
     }
